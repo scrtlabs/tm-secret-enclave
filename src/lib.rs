43418a059eae377dcf5ef9b3938586cb1a447904
TomL94 <langer.tom7@gmail.com>
2023-01-02 16:27:34 +0200

    Use the fixed tendermint-rs (our fork)

--- src/lib.rs
+++ src/lib.rs
@@ -3,24 +3,12 @@ mod error;
 mod logger;
 mod memory;
 
+use crate::enclave::functions::{health_check, random_number};
+use crate::error::{clear_error, set_error, Error};
+use ctor::ctor;
 use enclave::functions::next_validator_set;
 use logger::get_log_level;
-pub use memory::Buffer;
-
-// use std::convert::TryInto;
-// use std::panic::{catch_unwind, AssertUnwindSafe};
-// use std::str::from_utf8;
-
-use crate::error::{
-    clear_error,
-    // handle_c_error, handle_c_error_default,
-    set_error,
-    Error,
-};
-
-use ctor::ctor;
-// use log::*;
-use crate::enclave::functions::{health_check, random_number};
+use memory::Buffer;
 
 #[ctor]
 fn init_logger() {
