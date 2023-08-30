abdc799ff147c6a63b004f55901cb0361c67127c
Itzik Grossman <itzik@scrtlabs.com>
2023-08-30 19:00:34 +0300

    Revert "Compile with Ubuntu 20 and remove epid link (maybe it will work?)"
    This reverts commit 0040e16ddfdb37a99e7e3fea8c7948e718df1db3.

--- api/link_std.go
+++ api/link_std.go
@@ -2,5 +2,5 @@
 
 package api
 
-// #cgo LDFLAGS: -Wl,-rpath,${SRCDIR} -L${SRCDIR} -lrandom_api
+// #cgo LDFLAGS: -Wl,-rpath,${SRCDIR} -L${SRCDIR} -lrandom_api // -lsgx_epid -- add this back when compiling with 2.20
 import "C"
