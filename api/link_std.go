d0eb6319498b44d0fdca216140c800664041fdb0
Cashmaney <itzik@scrtlabs.com>
2024-01-15 21:18:35 +0200

    Update link_std.go

--- api/link_std.go
+++ api/link_std.go
@@ -2,5 +2,7 @@
 
 package api
 
-// #cgo LDFLAGS: -Wl,-rpath,${SRCDIR} -L${SRCDIR} -lrandom_api // -lsgx_epid -- add this back when compiling with 2.20
+//// -lsgx_epid -- add this back when compiling with 2.20
+
+// #cgo LDFLAGS: -Wl,-rpath,${SRCDIR} -L${SRCDIR} -lrandom_api 
 import "C"
