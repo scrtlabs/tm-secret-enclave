439caed3618f333f76251e17030166dabaa74fd4
Ilya Raykker <ilya.th.kapitonov@gmail.com>
2024-07-25 17:38:01 +0400

    Update rust-toolchain and dependencies

--- packages/enclave/src/validator_set.rs
+++ packages/enclave/src/validator_set.rs
@@ -5,11 +5,12 @@ use enclave_utils::validator_set::ValidatorSetForHeight;
 use log::{debug, error};
 use tendermint::Hash;
 use tendermint_proto::Protobuf;
+use tendermint_proto::v0_38::types::ValidatorSet as RawValidatorSet;
 
 pub fn get_validator_set_hash() -> SgxResult<Hash> {
     let res = ValidatorSetForHeight::unseal()?; //unseal(&VALIDATOR_SET_SEALING_PATH)?;
 
-    let hash = match tendermint::validator::Set::decode(&*(res.validator_set)) {
+    let hash = match <tendermint::validator::Set as Protobuf<RawValidatorSet>>::decode(&*(res.validator_set)) {
         Ok(vs) => {
             debug!("decoded validator set hash: {:?}", vs.hash());
             vs.hash()
