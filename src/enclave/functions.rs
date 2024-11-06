8af0480920f719fea79d2966a118af718dd86cf2
vlad <vlad@scrtlabs.com>
2024-11-06 16:39:52 +0000

    dbg(2)

--- src/enclave/functions.rs
+++ src/enclave/functions.rs
@@ -2,6 +2,11 @@ use crate::enclave::enclave_api::{ecall_generate_random, ecall_submit_validator_
 use crate::Error;
 use sgx_types::{sgx_status_t, SgxResult, sgx_enclave_id_t};
 
+
+use libc::{dlsym, RTLD_DEFAULT, c_void};
+use std::ffi::CString;
+use std::ptr::{null, null_mut};
+
 static mut S_EID: Option<sgx_enclave_id_t> = None;
 
 pub fn set_enclave(eid: u64) {
@@ -22,40 +27,46 @@ fn get_enclave() -> Result<sgx_enclave_id_t, crate::Error> {
     }
 }
 
+//type Pfn_random_number = unsafe extern "C" fn(block_hash: &[u8], height: u64) -> Result<Vec<u8>, crate::Error>;
+//static mut S_PFN_RANDOM_NUMBER: Option<Pfn_random_number> = None;
+
+type Symbol = *mut c_void;
+
+fn ensure_symbol_found(name: &str, p_symbol: &mut Symbol) -> bool {
+
+    if (*p_symbol).is_null() {
+
+        let symbol_name = CString::new(name).unwrap();
+        *p_symbol = unsafe { dlsym(RTLD_DEFAULT, symbol_name.as_ptr()) };
+
+        if (*p_symbol).is_null() {
+            println!("### symbol {} not found", name);
+            return false;
+        }
+        println!("### symbol {} loaded", name);
+    }
+    true
+}
+
+static mut S_PFN_RANDOM_NUMBER: Symbol = null_mut();
+
+
 pub fn random_number(block_hash: &[u8], height: u64) -> Result<Vec<u8>, crate::Error> {
 
-    println!("##### TM random_number");
+    unsafe {
 
-    let eid = get_enclave()?;
-    let mut retval = sgx_status_t::SGX_SUCCESS;
+        if !ensure_symbol_found("secret_impl_random_number", &mut S_PFN_RANDOM_NUMBER) {
+            return Err(Error::RandomGeneration { msg: "status unexpected".to_string() });
+        }
+        
 
-    let mut random = [0u8; 48];
-    let mut proof = [0u8; 32];
+        // Cast the raw pointer to the correct function type
+        type Pfn = unsafe extern "C" fn(block_hash: &[u8], height: u64) -> Result<Vec<u8>, crate::Error>;
+        let function: Pfn = std::mem::transmute(S_PFN_RANDOM_NUMBER);
 
-    let status = unsafe { ecall_generate_random(
-        eid,
-        &mut retval,
-        block_hash.as_ptr(),
-        block_hash.len() as u32,
-        height,
-        &mut random,
-        &mut proof,
-    ) };
+        function(block_hash, height)
 
-    println!("##### TM random_number ret={}, status={}", retval, status);
-
-    if retval != sgx_status_t::SGX_SUCCESS {
-        return Err(Error::RandomGeneration { msg: "retval unexpected".to_string() });
     }
-
-    if status != sgx_status_t::SGX_SUCCESS {
-        return Err(Error::RandomGeneration { msg: "status unexpected".to_string() });
-    }
-
-    let mut return_val = vec![];
-    return_val.extend_from_slice(&random);
-    return_val.extend_from_slice(&proof);
-    return Ok(return_val);
 }
 
 pub fn next_validator_set(val_set: &[u8], height: u64) -> SgxResult<()> {
